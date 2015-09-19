f7781cfb71f84cf79c9908a29115760118746f72
Joao Nzango <joaonzangoii@hotmail.com>
2015-09-19 21:41:21 +0200

    add attachments to users *avatar and photos8

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_attachment  :avatar, accept: [:jpg, :png, :gif]
+  has_attachments :photos, maximum: 10
+
+  validates :avatar, presence: true
 end
